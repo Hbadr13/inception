950d7d7da0e4e92e200b8ce0cfe8dbc304bf2d71
Your Name <you@example.com>
2023-08-01 10:47:39 +0100

    container

--- volume/wordpress/wp-config.php
+++ volume/wordpress/wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'hbadr' );
 
 /** Database password */
-define( 'DB_PASSWORD', '12345' );
+define( 'DB_PASSWORD', 'hbadr' );
 
 /** Database hostname */
 define( 'DB_HOST', 'mariadb' );
--- wordpress/tools/script.sh
+++ wordpress/tools/script.sh
@@ -3,8 +3,9 @@ mkdir -p /var/www
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-/usr/local/bin/wp core download --allow-root --path=/var/www/wordpress
+wp core download --allow-root --path=/var/www/wordpress
 #  > /var/www/wordpress/wp-config.php
 mv /wp-config.php /var/www/wordpress
 mkdir -p /run/php
 /usr/sbin/php-fpm7.3 -F
+
